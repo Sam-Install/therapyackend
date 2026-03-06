503e16997a44cf06e5a832dfc08c813a5ee8c8ac
Sam-Install <samielDev25@outlook.com>
2026-03-06 01:37:44 -0800

    cors fix

--- config/cors.php
+++ config/cors.php
@@ -8,6 +8,8 @@ return [
 
     'allowed_origins' => [
         'http://localhost:5173',
+        'http://localhost:3000',
+        'https://therapy.netlify.app'
     ],
 
     'allowed_origins_patterns' => [],
@@ -18,6 +20,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
